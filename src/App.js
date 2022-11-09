f3b1f9d6580f248887c1506208fab791d4edbbc7
MZ-87 <zsmile@mail.ru>
2022-11-09 16:21:14 +0300

    added react-charts

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import { Chart } from "react-google-charts";
 
 function App() {
   return (
@@ -18,8 +19,18 @@ function App() {
           Learn React
         </a>
       </header>
+
+      <Chart
+  chartType="ScatterChart"
+  data={[["Age", "Weight"], [4, 5.5], [8, 12]]}
+  width="100%"
+  height="400px"
+  legendToggle
+/>
     </div>
   );
 }
 
 export default App;
+
+
