024c488f7c6a3bc3e9f30023837bec4c313bbb56
MZ-87 <zsmile@mail.ru>
2022-11-10 10:53:28 +0300

    added pie chart

--- src/App.js
+++ src/App.js
@@ -1,36 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
-import { Chart } from "react-google-charts";
+import {
+  Chart
+} from "react-google-charts";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export const data = [
+  ["Task", "Hours per Day"],
+  ["Работа", 9],
+  ["Домашние обязанности", 2],
+  ["Код", 2],
+  ["Логистика", 2],
+  ["Разное", 1.5],
+  ["Сон", 7.5],
+];
 
-      <Chart
-  chartType="ScatterChart"
-  data={[["Age", "Weight"], [4, 5.5], [8, 12]]}
-  width="100%"
-  height="400px"
-  legendToggle
-/>
-    </div>
+export const options = {
+  title: "Что я делаю каждый будний день",
+};
+
+export function App() {
+  return ( <
+    Chart chartType = "PieChart"
+    data = {
+      data
+    }
+    options = {
+      options
+    }
+    width = {
+      "100%"
+    }
+    height = {
+      "500px"
+    }
+    />
   );
 }
 
 export default App;
-
-
